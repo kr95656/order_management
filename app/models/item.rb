56ed979f0570cb0c912dc2fe1d3de586509da133
kr95656 <katmsg8239@gmail.com>
2020-07-04 17:53:03 +0900

    Create size model of active hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
-  belongs_to :category                #カテゴリー
-  belongs_to :tax                     #税金
-  belongs_to :expiration_date         #賞味期限
-  belongs_to :processing              #加工日
+  belongs_to :category                        #カテゴリー
+  belongs_to :tax                             #税金
+  belongs_to :expiration_date                 #賞味期限
+  belongs_to :processing                      #加工日
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :shipping_prefecture #産地
-  belongs_to_active_hash :weight #重量
-  belongs_to_active_hash :size #サイズ
-  belongs_to_active_hash :shipping_day #到着までの期間
+  belongs_to_active_hash :weight              #重量
+  belongs_to_active_hash :size                #サイズ
+  belongs_to_active_hash :shipping_day        #到着までの期間
 end
