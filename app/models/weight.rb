cd8eec27aa5f35c4b5112b8464c8b990d95db21c
kr95656 <katmsg8239@gmail.com>
2020-07-04 18:00:06 +0900

    Create weight model of active hash

---
+++ app/models/weight.rb
@@ -0,0 +1,16 @@
+class Weight < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '1 ~ 5 kg'}, 
+    {id: 2, name: '6 ~ 10 kg'}, 
+    {id: 3, name: '11 ~ 15 kg'}, 
+    {id: 4, name: '16 ~ 20 kg'}, 
+    {id: 5, name: '21 ~ 25 kg'}, 
+    {id: 6, name: '26 ~ 30 kg'}, 
+    {id: 7, name: '31 ~ 35 kg'}, 
+    {id: 8, name: '36 ~ 40 kg'}, 
+    {id: 9, name: '41 ~ 45 kg'}, 
+    {id: 10, name: '46 ~ 50 kg'}, 
+    {id: 11, name: '51 ~ 55 kg'}, 
+    {id: 12, name: '56 ~ 60 kg'}, 
+  ]
+end
