01da018bb6182bfb55a7ef7fe77bb67ab83a4a5a
kr95656 <katmsg8239@gmail.com>
2020-07-04 17:35:24 +0900

    Add association to item model

--- db/migrate/20200703074850_create_items.rb
+++ db/migrate/20200703074850_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :shipping_prefecture, null: false                       #産地
       t.references :tax, null: false, foreign_key: true                 #税金
       t.references :processing, null: false, foreign_key: true          #加工日
-      t.references :shipping_day, null: false, foreign_key: true        #到着日までの期間 
+      t.references :shipping_day, null: false, foreign_key: true        #到着までの期間 
       t.references :expiration_date, null: false, foreign_key: true     #賞味期限
       t.references :category, null: false, foreign_key: true            #カテゴリー
       t.timestamps
