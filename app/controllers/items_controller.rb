f4ab87cc790e85ad50f525bdce21e064290c0b0b
kr95656 <katmsg8239@gmail.com>
2020-07-05 18:17:50 +0900

    add item.create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,26 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+
+
+  # def create
+  #   @item = Item.create(item_params)
+  #   if @item.save
+  #     flash[:notice] = "投稿完了しました"
+  #     redirect_to root_path
+  #   else
+  #     flash.now[:alert] = "投稿に失敗しました"
+  #     render ("items/new")
+  #   end
+  # end
+
 
   private
   
