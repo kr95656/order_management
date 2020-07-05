b63e3ef2f0681cdf0557430f1b97d6a1d0b3fcf9
kr95656 <katmsg8239@gmail.com>
2020-07-05 17:37:09 +0900

    add create_action to create controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,14 @@
 class ItemsController < ApplicationController
   
-  def index
-    
-  end
+  def index; end
 
   def new
     @item = Item.new
   end
 
   def create
-    
+    @item = Item.create(item_params)
   end
+
+  
 end
