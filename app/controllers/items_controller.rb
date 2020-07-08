2d2aaf49ca5d40887592e4580227cea6574f9378
kr95656 <katmsg8239@gmail.com>
2020-07-08 22:56:02 +0900

    seach factor to can't save db now

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,20 @@ class ItemsController < ApplicationController
   
   def index; end
 
+
+#createを設定しているが、機能していない。つまり、create以前で問題点があると考えられる。
+#newコントローラーがおかしいのか？
+#gem関係なのか？
+#rubyのバージョンの問題なのか？
+#次回も、データベースに保存されない原因を追求する。
   def new
-    item = Item.new
+    @item = Item.new
     @parents = Category.all.order("id ASC").limit(13)
   end
 
   def create
-    # @item = Item.create(item_params)
-    item = Item.new(item_params)
+    binding.pry #データを保存するメソッドの中に
+    item = Item.create(item_params)
     if item.save
       redirect_to root_path
     else
