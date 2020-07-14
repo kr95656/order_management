de2b52d1d96496743551efd56e4f21d45b3c4454
kr95656 <katmsg8239@gmail.com>
2020-07-14 22:55:57 +0900

    item comment out

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,21 +25,21 @@ class ItemsController < ApplicationController
 end
 
 
-From: /Users/kamimura/order/order_management/app/controllers/items_controller.rb:10 ItemsController#create:
+# From: /Users/kamimura/order/order_management/app/controllers/items_controller.rb:10 ItemsController#create:
 
-     9: def create
- => 10:   binding.pry
-    11:   @item = Item.new(item_params)
-    12:   if @item.save
-    13:     redirect_to root_path
-    14:   else
-    15:     render :new
-    16:   end
-    17: end
+#      9: def create
+#  => 10:   binding.pry
+#     11:   @item = Item.new(item_params)
+#     12:   if @item.save
+#     13:     redirect_to root_path
+#     14:   else
+#     15:     render :new
+#     16:   end
+#     17: end
 
-[1] pry(#<ItemsController>)> params
-=> <ActionController::Parameters {"authenticity_token"=>"9p8+RPmZJslyKamdDh1r4rky25vNJR7K5jYZXyd9jq0Y8Reh56Yd72CMgmjYNaWaGSgdDgyCpARFFvLW1FODag==", "item"=>{"name"=>"fff", "tax"=>"1", "price"=>"-1"}, "commit"=>"送信", "controller"=>"items", "action"=>"create"} permitted: false>
-[2] pry(#<ItemsController>)> item_params
-=> <ActionController::Parameters {"price"=>"-1", "name"=>"fff", "tax"=>"1"} permitted: true>
-[3] pry(#<ItemsController>)> 
+# [1] pry(#<ItemsController>)> params
+# => <ActionController::Parameters {"authenticity_token"=>"9p8+RPmZJslyKamdDh1r4rky25vNJR7K5jYZXyd9jq0Y8Reh56Yd72CMgmjYNaWaGSgdDgyCpARFFvLW1FODag==", "item"=>{"name"=>"fff", "tax"=>"1", "price"=>"-1"}, "commit"=>"送信", "controller"=>"items", "action"=>"create"} permitted: false>
+# [2] pry(#<ItemsController>)> item_params
+# => <ActionController::Parameters {"price"=>"-1", "name"=>"fff", "tax"=>"1"} permitted: true>
+# [3] pry(#<ItemsController>)> 
 
