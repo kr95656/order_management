3e886c2635d8c09956bc292098c5f5f8c4f881ed
kr95656 <katmsg8239@gmail.com>
2020-07-16 18:46:29 +0900

    add activehash association to item_model

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
   # belongs_to :processing                      #加工日
   # belongs_to :user                            #ユーザー
 
-  # extend ActiveHash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # belongs_to_active_hash :shipping_prefecture #産地
   # belongs_to_active_hash :weight              #重量
-  # belongs_to_active_hash :size                #サイズ
+  belongs_to_active_hash :size                #サイズ
   # belongs_to_active_hash :shipping_day        #到着までの期間
 end
