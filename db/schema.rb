7c1e3ef48e6cc4e2789e73d817ccbb36643dc1d2
kr95656 <katmsg8239@gmail.com>
2020-07-17 16:48:08 +0900

    add weight_id in item_tables

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2020_07_03_074850) do
     t.integer "tax", null: false
     t.integer "price", null: false
     t.string "size_id", null: false
+    t.string "weight_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
