ff3c5d2f4901281be65f72ce20b4cccdb280bfc3
kr95656 <katmsg8239@gmail.com>
2020-07-22 18:08:45 +0900

    add shipping_day_id item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:price, :name, :tax, :size_id, :weight_id, :shipping_prefecture_id)
+    params.require(:item).permit(:price, :name, :tax, :size_id, :weight_id, :shipping_prefecture_id, :shipping_day_id)
   end
 
 end
