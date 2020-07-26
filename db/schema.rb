8e2953de9aeef72bacdd9fa8a72d78d505dbb9a2
kr95656 <katmsg8239@gmail.com>
2020-07-26 16:35:08 +0900

    rails db:migrate RAILS_ENV=development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_071505) do
+ActiveRecord::Schema.define(version: 2020_07_26_073133) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 2020_07_26_071505) do
     t.date "expiration_date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
+  add_foreign_key "items", "categories"
 end
