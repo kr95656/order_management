70725cb3e9332c2a9ef32e96c7cd14344068db85
kr95656 <katmsg8239@gmail.com>
2020-07-29 22:50:30 +0900

    get parent_category

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,39 +1,5 @@
 $(function(){
-  $('#parent_category').on('change', function(){
-    var parentCategory = document.getElementById('parent_category').value;
-
-    if(parentCategory !== "---") {
-      $.ajax({
-        url: 'get_category_children', 
-        type: 'GET',
-        // data: {parent_name: parentCategory} → children
-        data: {parent_name: parentCategory}, 
-        dataType: 'json'
-      })
-      .done(function(children){
-        $('#children_wrapper').remove();
-        $('#grandchildren_wrapper').remove();
-        $('#size_wrapper').remove();
-        $('#brand_wrapper').remove();
-        var insertHTML = '';
-        children.forEach(function(child){
-          insertHTML += appendOption(child);
-        });
-        appendChidrenBox(insertHTML);
-      })
-      .fail(function(){
-        alert('カテゴリー取得に失敗しました');
-      })
-    }else{
-      $('#children_wrapper').remove();
-      $('#grandchildren_wrapper').remove();
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
-    }
-  });
-
-
-
+  // 親カテゴリー選択後のイベント
   $('#parent_category').on('change', function(){
     var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
     if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
